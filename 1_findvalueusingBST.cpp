8706d683c357232341a518b6edbe6bf665f90bac
Priyanshu <priyanshu0102here@gmail.com>
2022-07-17 17:04:23 +0530

    function for finding the given binary tree is bst or not

--- 1_findvalueusingBST.cpp
+++ 1_findvalueusingBST.cpp
@@ -81,11 +81,46 @@ void findNode(binarytreenode<int>* root, int key){
 
 }
 
+
+//function to find is the binary tree a bst
+
+int maximum(binarytreenode<int>* root)
+{
+    if(root==NULL)
+    return INT_MIN;
+
+    return max(root->data, max((maximum(root->left)), maximum(root->right)));
+
+}
+
+int minimum(binarytreenode<int> * root)
+{
+
+    if(root==NULL)
+    return INT_MAX;
+
+    return min(root->data, min(minimum(root->left), minimum(root->right)));
+
+}
+
+bool isBST(binarytreenode<int> *root)
+{
+    if(root==NULL)
+    return true;
+
+int leftmax=maximum(root->left);
+int rightmin=minimum(root->right);
+return (root->data>leftmax) && (root->data<rightmin) && (isBST(root->left)) && (isBST(root->right));
+
+}
+// root 3 2 5 -1 -1 4 7 -1 -1 -1 -1
 int main()
 {
 
 binarytreenode<int>* root=takeinput();
 printtree(root);
-findNode(root,4);
+// findNode(root,4);
+cout<<endl;
+cout<<isBST(root);
     return 0;
 }
